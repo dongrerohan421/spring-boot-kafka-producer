ad1fa94f5af47de09cd109e5bb9960c7cbc0566a
dongrerohan421 <dongrerohan421@gmail.com>
2020-07-20 21:51:49 -0400

    add swagger

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # spring-boot-kafka-producer
+This is SpringBoot Kafka Producer app with REST API's developed in SpringBoot 2.3.1.RELEASE, Gradle and Swagger2.
+
+
+Please feel free to modify the server port. The current state uses 8081 as server port.
+
+Please follow this [URL](https://kafka.apache.org/quickstart) to download and start Zookeeper server.
+
+- **For APi docs:** Please visit http://localhost:8081/v2/api-docs
+- **To use swagger:** http://localhost:8081/swagger-ui.html#/
---
+++ src/main/java/com/kafka/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.kafka;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	public static final Contact DEFAULT_CONTACT = new Contact("Rohan Dongre", "", "dongrrohan421@gmail.com");
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("SpringBoot Kafka Producer API Documentation", "Publish messages using Spring Kafka.\n" +
+            "\n" +
+            "The goal of this project is to assess development skills suitable for a Spring Kafka by testing knowledge and proficiency with software design, unit testing and web technologies like HTTP and REST.",
+            "1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/text"));
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+    }
+}
